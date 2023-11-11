e57f4df57c7cacb7e4f76c749d1a0fc96433b106
Qusabe <113439301+Qusabe@users.noreply.github.com>
2023-11-12 00:58:29 +0300

    Update configs.java

--- src/main/java/com/example/test/configs.java
+++ src/main/java/com/example/test/configs.java
@@ -2,13 +2,16 @@ package com.example.test;
 
 
 // конфиги БД
-public class configs {
+public class configs {   // конфиги БД стоит заменить на конфиги твоей базы данных
     protected String dbHost = "127.0.0.1";
     protected String dbPort = "3306";
     protected String dbUser = "root";
     protected String dbPass = "12345";
     protected String dbName = "test";
 }
+
+// Ниже пример конфигов для удаленной БД (работало)
+
 //public class configs {
 //    protected String dbHost = "sql7.freesqldatabase.com";
 //    protected String dbPort = "3306";
